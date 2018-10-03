276953a4cf6b7f943eae12e32d3d0fbacbce884d
velocic <velocicdev@gmail.com>
2018-10-02 22:52:04 -0400

    Adding bash scripts & notes file for quick new machine dev environment setup

---
+++ .bashrc.d/bash-tweaks.sh
@@ -0,0 +1,29 @@
+# More easily search for c++ std library features from man pages (if already installed via stdman on github: https://github.com/jeaye/stdman.git
+ref()
+{
+    apropos . | grep -i std:: | grep -i "$1"
+}
+
+# Colors
+default=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+purple=$(tput setaf 5)
+orange=$(tput setaf 9)
+
+# Less colors for man pages
+export PAGER=less
+# Begin blinking
+export LESS_TERMCAP_mb=$red
+# Begin bold
+export LESS_TERMCAP_md=$orange
+# End mode
+export LESS_TERMCAP_me=$default
+# End standout-mode
+export LESS_TERMCAP_se=$default
+# Begin standout-mode - info box
+export LESS_TERMCAP_so=$purple
+# End underline
+export LESS_TERMCAP_ue=$default
+# Begin underline
+export LESS_TERMCAP_us=$green
